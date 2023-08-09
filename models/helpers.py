7b88da8aa33b044f08febebb44db18b2f6ed861f
Sandra Molina Palencia <smolinapalencia@gmail.com>
2023-08-09 10:14:02 +0100

    working on helpers

--- models/helpers.py
+++ models/helpers.py
@@ -4,7 +4,10 @@ from models.images import Images
 
 # Create an instance of the Settings class
 settings = Settings()
+screen = settings.screen
+
 images = Images()
+
 pygame.font.init()
 font = pygame.font.Font('freesansbold.ttf', 20)
 medium_font = pygame.font.Font('freesansbold.ttf', 40)
@@ -18,29 +21,29 @@ def draw_board():
         column = i % 4
         row = i // 4
         if row % 2 == 0:
-            pygame.draw.rect(settings.screen, 'light pink', [
+            pygame.draw.rect(screen, 'light pink', [
                              600 - (column * 200), row * 100, 100, 100])
         else:
-            pygame.draw.rect(settings.screen, 'light pink', [
+            pygame.draw.rect(screen, 'light pink', [
                              700 - (column * 200), row * 100, 100, 100])
-        pygame.draw.rect(settings.screen, 'violet', [
+        pygame.draw.rect(screen, 'violet', [
                          0, 800, settings.WIDTH, 100])
-        pygame.draw.rect(settings.screen, 'white', [
+        pygame.draw.rect(screen, 'white', [
                          0, 800, settings.WIDTH, 100], 5)
-        pygame.draw.rect(settings.screen, 'white', [
+        pygame.draw.rect(screen, 'white', [
                          800, 0, 200, settings.HEIGHT], 5)
         status_text = ['White: Select a Piece to Move!', 'White: Select a Destination!',
                        'Black: Select a Piece to Move!', 'Black: Select a Destination!']
-        settings.screen.blit(medium_font.render(
+        screen.blit(medium_font.render(
             status_text[settings.turn_step], True, 'black'), (20, 820))
 
         # Adds horizontal and vertical lines to the board
         for i in range(9):
-            pygame.draw.line(settings.screen, 'white',
+            pygame.draw.line(screen, 'white',
                              (0, 100 * i), (800, 100 * i), 2)
-            pygame.draw.line(settings.screen, 'white',
+            pygame.draw.line(screen, 'white',
                              (100 * i, 0), (100 * i, 800), 2)
-        settings.screen.blit(medium_font.render(
+        screen.blit(medium_font.render(
             'Forfeit', True, 'white'), (810, 830))
 
 #  draw pieces into the board
@@ -50,24 +53,24 @@ def draw_pieces():
     for i in range(len(settings.white_pieces)):
         index = settings.piece_list.index(settings.white_pieces[i])
         if settings.white_pieces[i] == 'pawn':
-            settings.screen.blit(
+            screen.blit(
                 images.white_pawn, (settings.white_locations[i][0] * 100 + 22, settings.white_locations[i][1] * 100 + 30))
         else:
-            settings.screen.blit(images.white_images[index], (
+            screen.blit(images.white_images[index], (
                 settings.white_locations[i][0] * 100 + 1, settings.white_locations[i][1] * 100 + 10))
         if settings.turn_step < 2:
             if settings.selection == i:
-                pygame.draw.rect(settings.screen, 'red', [settings.white_locations[i][0] * 100 + 1, settings.white_locations[i][1] * 100 + 1,
-                                                          100, 100], 2)
+                pygame.draw.rect(screen, 'red', [settings.white_locations[i][0] * 100 + 1, settings.white_locations[i][1] * 100 + 1,
+                                                 100, 100], 2)
     for i in range(len(settings.black_pieces)):
         index = settings.piece_list.index(settings.black_pieces[i])
         if settings.black_pieces[i] == 'pawn':
-            settings.screen.blit(
+            screen.blit(
                 images.black_pawn, (settings.black_locations[i][0] * 100 + 22, settings.black_locations[i][1] * 100 + 30))
         else:
-            settings.screen.blit(images.black_images[index], (
+            screen.blit(images.black_images[index], (
                 settings.black_locations[i][0] * 100 + 10, settings.black_locations[i][1] * 100 + 10))
         if settings.turn_step >= 2:
             if settings.selection == i:
-                pygame.draw.rect(settings.screen, 'blue', [settings.black_locations[i][0] * 100 + 1, settings.black_locations[i][1] * 100 + 1,
-                                                           100, 100], 2)
+                pygame.draw.rect(screen, 'blue', [settings.black_locations[i][0] * 100 + 1, settings.black_locations[i][1] * 100 + 1,
+                                                  100, 100], 2)
