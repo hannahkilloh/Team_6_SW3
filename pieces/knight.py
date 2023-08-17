c2b0afb87aff2f5253ddf8bb11a8d936d0fda6f4
heatherwatson96 <heatherbell96@icloud.com>
2023-08-17 15:44:00 +0100

    tests for king and pawn developed

--- pieces/knight.py
+++ pieces/knight.py
@@ -1,4 +1,4 @@
-class King:
+class Knight:
     def __init__(self, colour, current_position, image):
         self.__colour = colour  # private variables with __
         self.__current_position = current_position
@@ -20,18 +20,16 @@ class King:
 
         return self.__current_position
 
+    # check knight moves
     def calculate_valid_moves(self, white_locations, black_locations):
         moves_list = []
         if self.__colour == 'white':
             friends_list = white_locations
         else:
             friends_list = black_locations
-        # 8 squares to check for kings moves, they can go one square in any direction
-        targets = [(1, 0), (1, 1), (1, -1), (-1, 0), (-1, 1), (-1, -1), (0, 1), (0, -1)]
-        for i in range(len(targets)):
-            # get the current positions first coord + loop each targets first coord
-            # , then the current positions second coord plus each targets second coord
-            # loop each target for the length of targets to find if the tile is taken and by black or white
+        # 8 squares to check for knights moves, they can go two squares in one direction and one in another direction
+        targets = [(1, 2), (1, -2), (2, 1), (2, -1), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
+        for i in range(8):
             target = (self.__current_position[0] + targets[i][0], self.__current_position[1] + targets[i][1])
             if target not in friends_list and 0 <= target[0] <= 7 and 0 <= target[1] <= 7:
                 moves_list.append(target)
--- tests/test_king.py
+++ tests/test_king.py
@@ -3,16 +3,48 @@ from Team_6_SW3.pieces.king import King
 
 
 class KingTests(unittest.TestCase):
-    def test_first_valid_moves(self):
-        king = King('white', (0, 1), None)
+    def test_valid_black_moves(self):
+        king = King('black', (3, 7), None)
         valid_moves = king.calculate_valid_moves([], [])
-        self.assertEqual([(0, 2), (0, 3)], valid_moves)
+        self.assertCountEqual([(2, 7), (2, 6), (3, 6), (4, 6), (4, 7)], valid_moves)
+
+    def test_valid_white_moves(self):
+        king = King('white', (3, 0), None)
+        valid_moves = king.calculate_valid_moves([], [])
+        self.assertCountEqual([(2, 0), (2, 1), (3, 1), (4, 1), (4, 0)], valid_moves)
 
     def test_move(self):
-        pawn = King('white', (0, 1), None)
-        pawn.calculate_valid_moves([], [])
-        new_position = pawn.move_to_selected_position((0, 2))
-        self.assertEqual((0, 2), new_position)
+        king = King('black', (3, 5), None)
+        king.calculate_valid_moves([], [])
+        new_position = king.move_to_selected_position((3, 4))
+        self.assertEqual((3, 4), new_position)
+
+    # blocked in by black pieces protecting
+    def test_blocking_black_pieces(self):
+        king = King('black', (3, 7), None)
+        valid_moves = king.calculate_valid_moves([], [(2, 7), (2, 6), (3, 6), (4, 6), (4, 7)])
+        self.assertEqual([], valid_moves)
+
+    # blocked in by white pieces protecting
+    def test_blocking_white_pieces(self):
+        king = King('white', (3, 0), None)
+        valid_moves = king.calculate_valid_moves([(2, 0), (2, 1), (3, 1), (4, 1), (4, 0)], [])
+        self.assertEqual([], valid_moves)
+
+
+
+    # king edge of board movement
+    def test_valid_move_edge_of_board(self):
+        king = King('black', (0, 5), None)
+        valid_moves = king.calculate_valid_moves([], [])
+        self.assertCountEqual([(0, 4), (1, 4), (1, 5), (1, 6), (0, 6)], valid_moves)
+
+    # king corner of board
+    def test_corner_of_board(self):
+        king = King('black', (0, 7), None)
+        valid_moves = king.calculate_valid_moves([], [])
+        self.assertCountEqual([(0, 6), (1, 6), (1, 7)], valid_moves)
+
 
 
 if __name__ == '__main__':
