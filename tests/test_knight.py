7652dcf9ee453b90a5827d274775a11cca285f8e
heatherwatson96 <heatherbell96@icloud.com>
2023-08-20 18:05:31 +0100

    fixed tests to instantiate piece objects correctly

--- tests/test_knight.py
+++ tests/test_knight.py
@@ -1,28 +1,29 @@
 import unittest
-from Team_6_SW3.pieces.knight import Knight
+from Team_6_SW3.models.pieces.knight import Knight
 
 
 class KnightTests(unittest.TestCase):
     # tests the knights first move
     def test_first_valid_moves(self):
-        knight = Knight('white', (1, 0), None)
+        knight = Knight('white', (1, 0))
         valid_moves = knight.calculate_valid_moves([], [])
         self.assertEqual([(2, 2), (3, 1), (0, 2)], valid_moves)
 
     def test_move(self):
-        knight = Knight('white', (1, 0), None)
+        knight = Knight('white', (1, 0))
         knight.calculate_valid_moves([], [])
         new_position = knight.move_to_selected_position((2, 2))
         self.assertEqual((2, 2), new_position)
 
     def test_invalid_move(self):
-        knight = Knight('white', (1, 0), None)
+        knight = Knight('white', (1, 0))
         knight.calculate_valid_moves([], [])
         new_position = knight.move_to_selected_position((0, 3))
         self.assertNotEqual((0, 3), new_position)
 
 # todo:
 # test he can jump a pawn
+
 
 if __name__ == '__main__':
     unittest.main()
