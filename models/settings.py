467f90208449be5c11d0d650d1e6b9b4ece1f675
heatherwatson96 <heatherbell96@icloud.com>
2023-08-20 18:06:34 +0100

    removed rook, knight and king from the old pieces arrays in settings file

--- models/settings.py
+++ models/settings.py
@@ -1,4 +1,9 @@
-from Team_6_SW3.pieces.pawn import Pawn
+from Team_6_SW3.models.pieces.pawn import Pawn
+from Team_6_SW3.models.pieces.king import King
+from Team_6_SW3.models.pieces.knight import Knight
+from Team_6_SW3.models.pieces.rook import Rook
+
+
 import pygame
 
 pygame.init()
@@ -17,30 +22,36 @@ class Settings:
 
         # Pieces in the board
         self.piece_list = ['pawn', 'queen', 'king', 'knight', 'rook', 'bishop']
-        self.white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook']
+        self.white_pieces = ['bishop', 'queen', 'bishop']
 
         # Coordinates for pieces
-        self.white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)]
+        self.white_locations = [(2, 0), (3, 0), (5, 0)]
 
-        self.black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook']
+        self.black_pieces = ['bishop', 'queen', 'bishop']
 
-        self.black_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7)]
+        self.black_locations = [(2, 7), (3, 7), (5, 7)]
 
         self.black_piece_objects = [Pawn('black', (0, 6)), Pawn('black', (1, 6)),
                                     Pawn('black', (2, 6)), Pawn('black', (3, 6)),
                                     Pawn('black', (4, 6)), Pawn('black', (5, 6)),
                                     Pawn('black', (6, 6)), Pawn('black', (7, 6)),
 
-                                    # King('black', (0, 6)), King('black', (1, 6)),
-                                    # King('black', (2, 6)), King('black', (3, 6)),
-                                    # King('black', (4, 6)), King('black', (5, 6)),
-                                    # King('black', (6, 6)), King('black', (7, 6))
+                                    King('black', (4, 7)),
+                                    Knight('black', (1, 7)), Knight('black', (6, 7)),
+                                    Rook('black', (7, 7)), Rook('black', (0, 7)),
+
                                     ]
 
         self.white_piece_objects = [Pawn('white', (0, 1)), Pawn('white', (1, 1)),
                                     Pawn('white', (2, 1)), Pawn('white', (3, 1)),
                                     Pawn('white', (4, 1)), Pawn('white', (5, 1)),
-                                    Pawn('white', (6, 1)), Pawn('white', (7, 1))]
+                                    Pawn('white', (6, 1)), Pawn('white', (7, 1)),
+
+                                    King('white', (4, 0)),
+                                    Knight('white', (1, 0)), Knight('white', (6, 0)),
+                                    Rook('white', (0, 0)), Rook('white', (7, 0)),
+
+                                    ]
 
         # Lists to keep track of captured pieces per team
         self.captured_pieces_white = []
