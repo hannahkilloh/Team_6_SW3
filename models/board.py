2d884bb43deb7241192c87b498e15401ed299041
heatherwatson96 <heatherbell96@icloud.com>
2023-08-21 23:14:30 +0100

    fixed indentation on board file

--- models/board.py
+++ models/board.py
@@ -2,6 +2,7 @@ from Team_6_SW3.models.pieces.pawn import Pawn
 
 import pygame
 
+
 class BoardSettings:
     def __init__(self, tile_colour_1, tile_colour_2, status_text_background, tile_border_colour,
                  font):
@@ -34,39 +35,39 @@ class Board:
     #  Function that will create the tiles of the board (8x8)
     def draw_board(self):
 
-            for i in range(32):
-                column = i % 4
-                row = i // 4
-                if row % 2 == 0:
-                    pygame.draw.rect(self.settings.screen, self.board_settings.tile_colour_2, [
-                        600 - (column * 200), row * 100, 100, 100])
-                else:
-                    pygame.draw.rect(self.settings.screen, self.board_settings.tile_colour_2, [
-                        700 - (column * 200), row * 100, 100, 100])
-            # pygame.draw.rect(self.settings.screen, self.board_settings.status_text_background, [
-            #                  800, 0, self.settings.WIDTH, 100])
-            # pygame.image.load(self.images("assets/images/test_background_image.png"))
-            # pygame.draw.rect(self.settings.screen, self.board_settings.status_text_background, [
-            #                  0, 800, self.settings.WIDTH, 100])
-            # pygame.draw.rect(self.settings.screen, self.board_settings.tile_border_colour, [
-            #                  0, 800, self.settings.WIDTH, 100], 2)
-            # pygame.draw.rect(self.settings.screen, self.board_settings.tile_border_colour, [
-            #                  800, 0, 200, self.settings.HEIGHT], 2)
-            # pygame.draw.rect(self.settings.screen, self.board_settings.status_text_background, [
-            #     0, 800, 200, self.settings.HEIGHT], 2)
-            status_text = ['White: Select a Piece to Move!', 'White: Select a Destination!',
-                           'Black: Select a Piece to Move!', 'Black: Select a Destination!']
-            self.settings.screen.blit(self.board_settings.medium_font.render(
-                status_text[self.settings.turn_step], True, 'blue'), (20, 820))
+        for i in range(32):
+            column = i % 4
+            row = i // 4
+            if row % 2 == 0:
+                pygame.draw.rect(self.settings.screen, self.board_settings.tile_colour_2, [
+                    600 - (column * 200), row * 100, 100, 100])
+            else:
+                pygame.draw.rect(self.settings.screen, self.board_settings.tile_colour_2, [
+                    700 - (column * 200), row * 100, 100, 100])
+        # pygame.draw.rect(self.settings.screen, self.board_settings.status_text_background, [
+        #                  800, 0, self.settings.WIDTH, 100])
+        # pygame.image.load(self.images("assets/images/test_background_image.png"))
+        # pygame.draw.rect(self.settings.screen, self.board_settings.status_text_background, [
+        #                  0, 800, self.settings.WIDTH, 100])
+        # pygame.draw.rect(self.settings.screen, self.board_settings.tile_border_colour, [
+        #                  0, 800, self.settings.WIDTH, 100], 2)
+        # pygame.draw.rect(self.settings.screen, self.board_settings.tile_border_colour, [
+        #                  800, 0, 200, self.settings.HEIGHT], 2)
+        # pygame.draw.rect(self.settings.screen, self.board_settings.status_text_background, [
+        #     0, 800, 200, self.settings.HEIGHT], 2)
+        status_text = ['White: Select a Piece to Move!', 'White: Select a Destination!',
+                       'Black: Select a Piece to Move!', 'Black: Select a Destination!']
+        self.settings.screen.blit(self.board_settings.medium_font.render(
+            status_text[self.settings.turn_step], True, 'blue'), (20, 820))
 
-            # Adds horizontal and vertical lines to the board
-            # for line in range(9):
-            # pygame.draw.line(self.settings.screen, self.board_settings.tile_border_colour,
-            #                  (0, 100 * line), (800, 100 * line), 2)
-            # pygame.draw.line(self.settings.screen, self.board_settings.tile_border_colour,
-            #                  (100 * line, 0), (100 * line, 800), 2)
-            self.settings.screen.blit(self.board_settings.medium_font.render(
-                'Resign?', True, 'blue'), (810, 830))
+        # Adds horizontal and vertical lines to the board
+        # for line in range(9):
+        # pygame.draw.line(self.settings.screen, self.board_settings.tile_border_colour,
+        #                  (0, 100 * line), (800, 100 * line), 2)
+        # pygame.draw.line(self.settings.screen, self.board_settings.tile_border_colour,
+        #                  (100 * line, 0), (100 * line, 800), 2)
+        self.settings.screen.blit(self.board_settings.medium_font.render(
+            'Resign?', True, 'blue'), (810, 830))
 
     def draw_move_suggestions(self, colour, potential_moves):
         for i in range(len(potential_moves)):
@@ -98,8 +99,8 @@ class Board:
             if self.settings.turn_step < 2:
                 if self.settings.selection == i:
                     pygame.draw.rect(self.settings.screen, 'black', [self.settings.white_locations[i][0] * 100 + 1,
-                                                                    self.settings.white_locations[i][1] * 100 + 1,
-                                                                    100, 100], 2)
+                                                                     self.settings.white_locations[i][1] * 100 + 1,
+                                                                     100, 100], 2)
 
 
 # ============================= new code
@@ -113,12 +114,13 @@ class Board:
         if self.settings.selected_piece is not None:
             selected_piece_is_white = self.settings.selected_piece.get_colour() == 'white'
             colour = 'red' if selected_piece_is_white else 'blue'
-            pygame.draw.rect(self.settings.screen, colour, [self.settings.selected_piece.get_current_position()[0] * 100 + 1,
-                                                           self.settings.selected_piece.get_current_position()[1] * 100 + 1,
-                                                           100, 100], 2)
+            pygame.draw.rect(self.settings.screen, colour, [self.settings.selected_piece.get_current_position()[0]
+                                                            * 100 + 1,
+                                                            self.settings.selected_piece.get_current_position()[1]
+                                                            * 100 + 1,
+                                                            100, 100], 2)
             self.draw_move_suggestions(colour, self.settings.selected_piece.get_valid_moves())
 # ============================= end of new code
-
 
         for i in range(len(self.settings.black_pieces)):
             index = self.settings.piece_list.index(self.settings.black_pieces[i])
@@ -134,4 +136,3 @@ class Board:
                     pygame.draw.rect(self.settings.screen, 'red', [self.settings.black_locations[i][0] * 100 + 1,
                                                                    self.settings.black_locations[i][1] * 100 + 1,
                                                                    100, 100], 2)
-
