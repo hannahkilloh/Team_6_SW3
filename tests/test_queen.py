d1364765653d594357557ac1be46994d9487bf9c
heatherwatson96 <heatherbell96@icloud.com>
2023-08-21 23:13:25 +0100

    added test file for queen

---
+++ tests/test_queen.py
@@ -0,0 +1,9 @@
+import unittest
+from Team_6_SW3.models.pieces.queen import Queen
+
+
+class QueenTests(unittest.TestCase):
+    def test_valid_black_moves(self):
+        queen = Queen('black', (3, 7))
+        valid_moves = queen.calculate_valid_moves([], [])
+        self.assertCountEqual([(2, 7), (2, 6), (3, 6), (4, 6), (4, 7)], valid_moves)
