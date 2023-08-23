5e8f81bb85e8d93513cf3b0d661ed15c380dadef
Hannah Killoh <128159869+hannahkilloh@users.noreply.github.com>
2023-08-23 18:15:01 +0100

    adding history class for moves history for castling and for populating json chess game history file

---
+++ Chess_Battle/models/history.py
@@ -0,0 +1,17 @@
+import datetime
+
+class History:
+    def __init__(self, settings):
+        self.settings = settings
+        self.move_history = settings.load_json("moves")
+        self.cur_session = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+        self.move_history[self.cur_session] = {"black": [], "white": []}
+
+    def is_move_in_history(self, colour, move):
+        # convert the int x, y coords to chess notation before checking history dict
+        move = self.settings.compute_notation(move)
+        moves = self.move_history[self.cur_session][colour]
+        for m in moves:
+            if m[0] == move:
+                return True
+        return False
