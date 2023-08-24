c680ab4261348b8581b11da8c65aa775d095d364
heatherwatson96 <heatherbell96@icloud.com>
2023-08-24 10:19:30 +0100

    Update test_game.py

--- tests/test_game.py
+++ tests/test_game.py
@@ -4,6 +4,8 @@ import ..game
 
 class GameTests(unittest.TestCase):
     def test_get_object_coords(self):
+        self.piece = game('white', (0, 0), 'queen', (45, 45), (80, 80))
+        piece =
 
         self.assertEqual(True, False)  # add assertion here
 
