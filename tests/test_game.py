b3b87bbafd4ab73d2b743c56565e88c29081ab84
heatherwatson96 <heatherbell96@icloud.com>
2023-08-25 11:07:38 +0100

    correcting and adding pawn promotion tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,6 @@
 import unittest
-from game import get_object_coords, get_clicked_piece, get_all_object_coords, get_king
+from game import get_object_coords, get_clicked_piece, get_all_object_coords, pawn_promotion_for_white, get_king, \
+    pawn_promotion_for_black
 from models.pieces.pawn import Pawn
 from models.pieces.queen import Queen
 from models.pieces.king import King
@@ -10,12 +11,24 @@ from models.pieces.knight import Knight
 
 class GameTests(unittest.TestCase):
     def test_get_object_coords(self):
-        # Arrange - set up the data to be passed to the function
         piece = Pawn('white', (0, 0))
-        # Act - actually call the function and get the result
         coords = get_object_coords(piece)
-        # Assert - check that the result you got from the function call is as exoected
         self.assertEqual((0, 0), coords)
+
+    def test_off_board_coords_get_object_coords(self):
+        piece = Pawn('white', (100, 100))
+        coords = get_object_coords(piece)
+        self.assertEqual((100, 100), coords)
+
+    def test_invalid_get_object_coords(self):
+        piece = Pawn('white', (10, 10))
+        coords = get_object_coords(piece)
+        self.assertNotEqual((100, 100), coords)
+
+    def test_edgecase_black_get_object_coords(self):
+        piece = Rook('black', (7, 7))
+        coords = get_object_coords(piece)
+        self.assertEqual((7, 7), coords)
 
     def test_valid_get_clicked_piece(self):
         coords = (0, 1)
@@ -102,29 +115,6 @@ class GameTests(unittest.TestCase):
         piece = get_all_object_coords(piece_objects)
         self.assertCountEqual([], piece)
 
-# todo: check this
-    def test_pawn_promotion_for_white(self):
-        piece = Pawn('white', (6, 7))
-        piece_objects = [Pawn('white', (0, 1)), Pawn('white', (1, 1)), Pawn('white', (2, 1)), Pawn('white', (3, 1)),
-                         Pawn('white', (4, 1)), Pawn('white', (5, 1)), Pawn('white', (6, 1)), Pawn('white', (7, 1)),
-                         King('white', (3, 0)), Knight('white', (1, 0)), Knight('white', (6, 0)), Rook('white', (0, 0)),
-                         Rook('white', (7, 0)), Bishop('white', (2, 0)), Bishop('white', (5, 0)), Queen('white', (4, 0))]
-        self.assertTrue(isinstance(piece, Pawn), piece)
-
-    def test_pawn_promotion_for_black(self):
-        piece = Pawn('black', (6, 7))
-        piece_objects = [Pawn('black', (0, 6)), Pawn('black', (1, 6)),
-                                    Pawn('black', (2, 6)), Pawn('black', (3, 6)),
-                                    Pawn('black', (4, 6)), Pawn('black', (5, 6)),
-                                    Pawn('black', (6, 6)), Pawn('black', (7, 6)),
-
-                                    King('black', (3, 7)),
-                                    Knight('black', (1, 7)), Knight('black', (6, 7)),
-                                    Rook('black', (7, 7)), Rook('black', (0, 7)),
-                                    Bishop('black', (2, 7)), Bishop('black', (5, 7)),
-                                    Queen('black', (4, 7))]
-        self.assertTrue(isinstance(piece, Pawn), piece)
-
     def test_white_get_king(self):
         piece = [King('white', (3, 0))]
         king = get_king(piece)
@@ -135,7 +125,61 @@ class GameTests(unittest.TestCase):
         king = get_king(piece)
         self.assertTrue(isinstance(king, King))
 
-# todo test castling onc ecoverted to fuunction in here
+    def test_invalid_get_king(self):
+        piece = [Pawn('white', (3, 0))]
+        pawn = get_king(piece)
+        self.assertFalse(isinstance(pawn, King))
+
+    def test_pawn_promotion_for_white(self):
+        coords = (6, 7)
+        piece = Pawn('white', (6, 7))
+        piece_objects = [Pawn('white', (6, 7)), Pawn('white', (1, 1)), Pawn('white', (2, 1)), Pawn('white', (3, 1)),
+                         Pawn('white', (4, 1)), Pawn('white', (5, 1)), Pawn('white', (6, 1)), Pawn('white', (7, 1)),
+                         King('white', (3, 0)), Knight('white', (1, 0)), Knight('white', (6, 0)), Rook('white', (0, 0)),
+                         Rook('white', (7, 0)), Bishop('white', (2, 0)), Bishop('white', (5, 0))]
+        pawn_promotion_for_white(coords, piece, piece_objects)
+        queen = piece_objects[len(piece_objects)-1]
+        pawn_promoted = isinstance(queen, Queen)
+        self.assertTrue(pawn_promoted)
+
+    def test_pawn_promotion_for_black(self):
+        coords = (6, 0)
+        piece = Pawn('black', (6, 0))
+        piece_objects = [Pawn('black', (0, 6)), Pawn('black', (1, 6)), Pawn('black', (2, 6)), Pawn('black', (3, 6)),
+                         Pawn('black', (4, 6)), Pawn('black', (5, 6)), Pawn('black', (6, 6)), Pawn('black', (7, 6)),
+                         King('black', (3, 7)), Knight('black', (1, 7)), Knight('black', (6, 7)), Rook('black', (7, 7)),
+                         Rook('black', (0, 7)), Bishop('black', (2, 7)), Bishop('black', (5, 7))]
+        pawn_promotion_for_black(coords, piece, piece_objects)
+        queen = piece_objects[len(piece_objects)-1]
+        pawn_promoted = isinstance(queen, Queen)
+        self.assertTrue(pawn_promoted)
+
+    def test_invalid_coord_for_pawn_promotion_for_black(self):
+        coords = (6, 2)
+        piece = Pawn('black', (6, 2))
+        piece_objects = [Pawn('black', (0, 6)), Pawn('black', (1, 6)), Pawn('black', (2, 6)), Pawn('black', (3, 6)),
+                         Pawn('black', (4, 6)), Pawn('black', (5, 6)), Pawn('black', (6, 6)), Pawn('black', (7, 6)),
+                         King('black', (3, 7)), Knight('black', (1, 7)), Knight('black', (6, 7)), Rook('black', (7, 7)),
+                         Rook('black', (0, 7)), Bishop('black', (2, 7)), Bishop('black', (5, 7))]
+        pawn_promotion_for_black(coords, piece, piece_objects)
+        queen = piece_objects[len(piece_objects)-1]
+        pawn_promoted = isinstance(queen, Queen)
+        self.assertFalse(pawn_promoted)
+
+    def test_invalid_piece_for_pawn_promotion_for_black(self):
+        coords = (6, 0)
+        piece = Rook('black', (6, 0))
+        piece_objects = [Pawn('black', (0, 6)), Pawn('black', (1, 6)), Pawn('black', (2, 6)), Pawn('black', (3, 6)),
+                         Pawn('black', (4, 6)), Pawn('black', (5, 6)), Pawn('black', (6, 6)), Pawn('black', (7, 6)),
+                         King('black', (3, 7)), Knight('black', (1, 7)), Knight('black', (6, 7)), Rook('black', (7, 7)),
+                         Rook('black', (0, 7)), Bishop('black', (2, 7)), Bishop('black', (5, 7))]
+        pawn_promotion_for_black(coords, piece, piece_objects)
+        queen = piece_objects[len(piece_objects)-1]
+        pawn_promoted = isinstance(queen, Queen)
+        self.assertFalse(pawn_promoted)
+
+
+# todo test castling once coverted to function in here
 
 
 if __name__ == '__main__':
